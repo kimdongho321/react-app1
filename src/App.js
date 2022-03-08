c8b96c14971dd841ee03479bbac8ad63066cca01
김동호 <ehdrh321@naver.com>
2022-03-08 18:00:04 +0900

    이벤트 만들기

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component{
     super(props);
     this.state = {
       mode:'read',
+      selected_content_id:2,
       Subject: {title:"Web", sub:"world wide web!"},
       welcome:{title:'welcome', desc:'Hello, React!!'},
       contents: [
@@ -24,8 +25,16 @@ class App extends Component{
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
     return (
     <div className="App">
@@ -36,17 +45,14 @@ class App extends Component{
         this.setState({mode:'welcome'});
       }.bind(this)}
       />
-      {/* <header>
-          <h1><a href="/" onClick={function(e){
-            console.log(e);
-            e.preventDefault();
-            this.setState({
-              mode:'welcome'
-            });
-          }.bind(this)}>{this.state.Subject.title}</a></h1>
-          {this.state.Subject.sub}
-      </header> */}
-      <TOC data={this.state.contents}/>
+      <TOC onChangePage={function(id){
+        this.setState({
+          mode:'read',
+          selected_content_id:Number(id)
+        });
+      }.bind(this)} 
+      data={this.state.contents}
+      />
       <Content title={_title} desc={_desc}/>
     </div>
   );
